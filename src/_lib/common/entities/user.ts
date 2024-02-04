7d1e228ac6c8c0426921cda208c1939acb02c074
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-04 22:35:05 +0100

    Move user entity to common lib

--- src/_lib/common/entities/user.ts
+++ src/_lib/common/entities/user.ts
@@ -1,12 +1,15 @@
-import { Entity, EntityId } from "../../_lib/core";
+import { Entity, EntityId } from "../../core";
 
-type UserId = EntityId<'userId'>;
-type UserProps = {
+export type UserId = EntityId<'userId'>;
+export type UserProps = {
     firstName: string;
     lastName: string;
     email: string;
 }
 
+export const toUserId = (id: string): UserId => {
+    return id as UserId;
+}
 
 export class User implements Entity<UserId, UserProps> {
     constructor(readonly id: UserId, readonly props: UserProps) {}
