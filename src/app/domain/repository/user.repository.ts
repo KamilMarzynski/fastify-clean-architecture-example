8d6ad0fab446deb764480ab952227b9070a8d408
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-04 21:33:47 +0100

    Rename lib to _lib

--- src/app/domain/repository/user.repository.ts
+++ src/app/domain/repository/user.repository.ts
@@ -1,4 +1,4 @@
-import { Repository } from "../../../libs/core";
+import { Repository } from "../../../_lib/core";
 import { User } from "../../entities/user";
 
 export type UserRepository = Repository<User>;
