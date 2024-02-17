c08dac1bcfdbe7b522d9e80a04111153f60e9cf5
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-17 09:38:47 +0100

    Refactor user.controller.ts to remove unused code and simplify data input handling

--- src/app/user/adapters/http/controllers/user.controller.ts
+++ src/app/user/adapters/http/controllers/user.controller.ts
@@ -1,41 +1,18 @@
-import Joi from "joi";
-
 import { Request, Response } from "../../../../../_lib/core/http";
-import { CreateUserInput, CreateUserUseCase } from "../../../ports/use-cases/create-user.use-case";
 
-const createUserDataSchema = Joi.object({
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    email: Joi.string().email().required(),
-});
+import { CreateUserUseCase } from "../../../ports/use-cases/create-user.use-case";
 
 export class UserController {
     constructor(private readonly createUserUseCase: CreateUserUseCase) {
     }
 
     async createUser(req: Request, res: Response) {
-        const data = await this.validate(req, res, createUserDataSchema);
-
-        const input: CreateUserInput = {
-            data,
-            context: {
-                foo: 'bar'
-            }
+        const input = {
+            data: req.body
         }
         const userId = await this.createUserUseCase.execute(input);
         return res.send({
             userId
         });
-    }
-
-    private async validate(req: Request, res: Response, schema: Joi.ObjectSchema): Promise<any> {
-        const validationResult = schema.validate(req.body);
-        if (validationResult.error) {
-            return res.status(400).send({
-                error: validationResult.error.details[0].message
-            });
-        }
-
-        return validationResult.value;
     }
 }
