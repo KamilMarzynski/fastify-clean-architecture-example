4ed70369f1e1ab8296f9c21a9ca1deae885c611c
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-19 21:01:39 +0100

    Refactor user module to improve modularity and separation of concerns

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,6 +4,9 @@ import { CreateUserUseCaseImpl } from './use-cases/create-user.use-case';
 import { HttpUserController } from './adapters/http/controllers/user.controller';
 import { UserRouter } from './adapters/http/routers/user.router';
 
+// TODO: should user module know about fastify?
+// should user module know about the database implementation?
+// maybe I should only inject those to user module and instatiate them higher?
 export class UserModule {
     constructor(private readonly server: FastifyInstance) {}
 
@@ -16,6 +19,11 @@ export class UserModule {
 
         // HTTP adapters
         const userController = new HttpUserController(createUser);
+
+        // TODO: maybe user module shoud export controllers
+        // so that app module can register them
+        // router is tightly to the server
+        // but controller is not
         const userRouter = new UserRouter(userController, this.server);
 
         userRouter.register();
