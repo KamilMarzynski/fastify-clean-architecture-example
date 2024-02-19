f0c1d55f0e8b435940e4e2afe122c5fe2eb34d60
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-19 20:39:42 +0100

    Refactor user creation and error handling

--- src/app/user/adapters/http/controllers/user.controller.ts
+++ src/app/user/adapters/http/controllers/user.controller.ts
@@ -1,24 +1,32 @@
 import { Request, Response } from "../../../../../_lib/core/http";
+import { isUseCaseError } from "../../../../../_lib/core/use-case";
 
 import { CreateUserUseCase } from "../../../ports/use-cases/create-user.use-case";
+import { USER_EXCEPTIONS } from "../../../use-cases/exceptions";
 
 export class HttpUserController {
     constructor(private readonly createUserUseCase: CreateUserUseCase) {
     }
 
     async createUser(req: Request, res: Response) {
+        try {
         const input = {
             data: req.body
         }
         const output = await this.createUserUseCase.execute(input);
 
-        if ('code' in output) {
-            // TODO: add custom domain errors and handle them here
+        if (isUseCaseError(output)) {
+            if (output.code === USER_EXCEPTIONS.USER_ALREADY_EXISTS.code) {
+                return res.status(409).send(output.message);
+            }
             return res.status(500).send(output.message);
         }
 
         return res.send({
             userId: output.result.id
         });
+        } catch (error: any) {
+            return res.status(500).send(error.message);
+        }
     }
 }
--- src/app/user/use-cases/create-user.use-case.ts
+++ src/app/user/use-cases/create-user.use-case.ts
@@ -1,7 +1,8 @@
 import { CreateUserInput, CreateUserOutput, CreateUserUseCase } from "../ports/use-cases/create-user.use-case";
 import { UserRepository } from "../ports/user.repository";
-import { User } from "../../../_lib/_sharedKernel/user/domain/user.entity";
+import { User } from "../../../_lib/_sharedKernel";
 import { toValue } from "../../../_lib/core/entityId";
+import { USER_EXCEPTIONS } from "./exceptions";
 
 export class CreateUserUseCaseImpl implements CreateUserUseCase {
 
@@ -10,6 +11,11 @@ export class CreateUserUseCaseImpl implements CreateUserUseCase {
 
     async execute(input: CreateUserInput): Promise<CreateUserOutput> {
         const { data } = input;
+
+        const userExists = await this.userRepository.findByEmail(data.email);
+        if (userExists) {
+            return USER_EXCEPTIONS.USER_ALREADY_EXISTS;
+        }
 
         const id = await this.userRepository.getNextId();
         const user = new User(id, { firstName: data.firstName, lastName: data.lastName, email: data.email });
