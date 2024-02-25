55c2571cc9de29934156b707358ef79c68c4e997
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-25 13:48:40 +0100

    Add FindUserByIdUserUseCaseImpl to UserModule

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,6 +3,7 @@ import { LocalUserRepository } from './adapters/db/repositories/local/user.repos
 import { CreateUserUseCaseImpl } from './use-cases/create-user.use-case';
 import { HttpUserController } from './adapters/http/controllers/user.controller';
 import { UserRouter } from './adapters/http/routers/user.router';
+import { FindUserByIdUserUseCaseImpl } from './use-cases/find-user-by-id.use-case';
 
 // TODO: should user module know about fastify?
 // should user module know about the database implementation?
@@ -16,13 +17,17 @@ export class UserModule {
 
         // Application use cases
         const createUser = new CreateUserUseCaseImpl(userRepository);
+        const findUserByIdUseCase = new FindUserByIdUserUseCaseImpl(userRepository);
 
         // HTTP adapters
-        const userController = new HttpUserController(createUser);
+        const userController = new HttpUserController({
+            createUserUseCase: createUser,
+            findUserByIdUseCase: findUserByIdUseCase
+        });
 
         // TODO: maybe user module shoud export controllers
         // so that app module can register them
-        // router is tightly to the server
+        // router is tightly bound to the server
         // but controller is not
         const userRouter = new UserRouter(userController, this.server);
 
