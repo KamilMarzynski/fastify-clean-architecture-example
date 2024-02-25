dd20f2c8d39d131d5073856a26213b699308c1e6
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-25 13:47:46 +0100

    Add FindUserByIdUserUseCase

---
+++ src/app/user/ports/use-cases/find-user-by-id.use-case.ts
@@ -0,0 +1,14 @@
+import { UseCase, UseCaseInput, UseCaseOutput } from "../../../../_lib/core"
+
+export type FindUserByIdUserInput = UseCaseInput<{
+    id: string,
+}>
+
+export type FindUserByIdUserOutput = UseCaseOutput<{
+    id: string,
+    firstName: string,
+    lastName: string,
+    email: string
+}>
+
+export type FindUserByIdUserUseCase = UseCase<FindUserByIdUserInput, FindUserByIdUserOutput>
