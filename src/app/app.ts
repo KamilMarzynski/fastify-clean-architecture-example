4c705afdc939aeccccb37baf45990b4bbc2c1104
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-25 20:29:56 +0100

    Remove todo comment

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,6 @@ type AppDependencies = {
     server: FastifyInstance
 }
 
-// TODO: change server to dependency container
 export const initApp = (deps: AppDependencies) => {
     const userModule = new UserModule(deps);
     userModule.init();
