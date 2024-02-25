26ada2e37b5fe5e47a581bd3fc674569750a61e0
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-25 13:48:48 +0100

    Add findUserById route schema

--- src/app/user/adapters/http/routers/user.router.ts
+++ src/app/user/adapters/http/routers/user.router.ts
@@ -11,13 +11,23 @@ const createUserSchema = {
             email: { type: 'string', format: 'email' }
         }
     }
+}
 
+const findUserByIdSchema = {
+    params: {
+        type: 'object',
+        required: ['id'],
+        properties: {
+            id: { type: 'string' }
+        }
+    }
 }
 
 export class UserRouter {
     constructor(private readonly userController: HttpUserController, private readonly server: FastifyInstance) { }
 
     public register() {
+        this.server.get('/users/:id', { schema: findUserByIdSchema }, (req, res) => this.userController.findUserById(req, res));
         this.server.post('/users', {
             schema: createUserSchema
         }, (req, res) => this.userController.createUser(req, res));
