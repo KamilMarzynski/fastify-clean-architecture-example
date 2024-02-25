d02dbf5a5361752f434ca83fac5a6ee3a756046c
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-25 13:48:01 +0100

    Add user not found exception

--- src/app/user/use-cases/exceptions.ts
+++ src/app/user/use-cases/exceptions.ts
@@ -4,5 +4,9 @@ export const USER_EXCEPTIONS: Record<string, UseCaseExceptionOutput> = {
     USER_ALREADY_EXISTS: {
         code: 'USER_001',
         message: 'User already exists'
+    },
+    USER_NOT_FOUND: {
+        code: 'USER_002',
+        message: 'User not found'
     }
 } as const;
