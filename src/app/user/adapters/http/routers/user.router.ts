9f2cc169579d4c8fa03053fbb69cf8b75948657d
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-27 19:47:39 +0100

    Add delete user endpoint to user router

--- src/app/user/adapters/http/routers/user.router.ts
+++ src/app/user/adapters/http/routers/user.router.ts
@@ -1,6 +1,14 @@
 import { HttpUserController } from "../controllers/user.controller";
 import { FastifyInstance } from "fastify";
 
+const userByIdParams = {
+    type: 'object',
+    required: ['id'],
+    properties: {
+        id: { type: 'string' }
+    }
+}
+
 const createUserSchema = {
     body: {
         type: 'object',
@@ -14,20 +22,20 @@ const createUserSchema = {
 }
 
 const findUserByIdSchema = {
-    params: {
-        type: 'object',
-        required: ['id'],
-        properties: {
-            id: { type: 'string' }
-        }
-    }
+    params: userByIdParams
 }
+
+const deleteUserSchema = {
+    params: userByIdParams
+}
+
 
 export class UserRouter {
     constructor(private readonly userController: HttpUserController, private readonly server: FastifyInstance) { }
 
     public register() {
         this.server.get('/users/:id', { schema: findUserByIdSchema }, (req, res) => this.userController.findUserById(req, res));
+        this.server.delete('/users/:id', { schema: deleteUserSchema }, (req, res) => this.userController.deleteUser(req, res));
         this.server.post('/users', {
             schema: createUserSchema
         }, (req, res) => this.userController.createUser(req, res));
