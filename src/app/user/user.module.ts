297bdc6a78da2f5ea8e77e2080da9c8460b8db52
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-27 19:47:21 +0100

    Add delete user functionality to UserModule

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { CreateUserUseCaseImpl } from './use-cases/create-user.use-case';
 import { HttpUserController } from './adapters/http/controllers/user.controller';
 import { UserRouter } from './adapters/http/routers/user.router';
 import { FindUserByIdUserUseCaseImpl } from './use-cases/find-user-by-id.use-case';
+import { DeleteUserUseCaseImpl } from './use-cases/delete-user.use-case';
 
 type UserModuleDependencies = {
     server: FastifyInstance
@@ -22,12 +23,14 @@ export class UserModule {
 
         // Application use cases
         const createUser = new CreateUserUseCaseImpl(userRepository);
-        const findUserByIdUseCase = new FindUserByIdUserUseCaseImpl(userRepository);
+        const findUserById = new FindUserByIdUserUseCaseImpl(userRepository);
+        const deleteUser = new DeleteUserUseCaseImpl(userRepository);
 
         // HTTP adapters
         const userController = new HttpUserController({
             createUserUseCase: createUser,
-            findUserByIdUseCase: findUserByIdUseCase
+            findUserByIdUseCase: findUserById,
+            deleteUserUseCase: deleteUser
         });
 
         // TODO: maybe user module shoud export controllers
