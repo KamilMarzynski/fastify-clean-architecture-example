8e893271470369f27d22771531551a827cc873a0
KamilMarzynski <kamil.marzynski@gmail.com>
2024-02-27 19:47:13 +0100

    Add deleteUser method to HttpUserController

--- src/app/user/adapters/http/controllers/user.controller.ts
+++ src/app/user/adapters/http/controllers/user.controller.ts
@@ -2,21 +2,25 @@ import { Request, Response } from "../../../../../_lib/core/http";
 import { isUseCaseError } from "../../../../../_lib/core/use-case";
 
 import { CreateUserUseCase } from "../../../ports/use-cases/create-user.use-case";
+import { DeleteUserUseCase } from "../../../ports/use-cases/delete-user.use-case";
 import { FindUserByIdUserUseCase } from "../../../ports/use-cases/find-user-by-id.use-case";
 import { USER_EXCEPTIONS } from "../../../use-cases/exceptions";
 
 export type HttpUserControllerDependencies = {
     createUserUseCase: CreateUserUseCase,
     findUserByIdUseCase: FindUserByIdUserUseCase
+    deleteUserUseCase: DeleteUserUseCase
 }
 
 export class HttpUserController {
     private createUserUseCase: CreateUserUseCase;
     private findUserByIdUseCase: FindUserByIdUserUseCase;
+    private deleteUserUseCase: DeleteUserUseCase;
     
     constructor(deps: HttpUserControllerDependencies) {
         this.createUserUseCase = deps.createUserUseCase;
         this.findUserByIdUseCase = deps.findUserByIdUseCase;
+        this.deleteUserUseCase = deps.deleteUserUseCase;
     }
 
     async createUser(req: Request, res: Response) {
@@ -56,6 +60,26 @@ export class HttpUserController {
         }
 
         return res.send(output.result);
+        } catch (error: any) {
+            return res.status(500).send(error.message);
+        }
+    }
+    
+    async deleteUser(req: Request, res: Response) {
+        try {
+            const input = {
+                data: req.params
+            }
+            const output = await this.deleteUserUseCase.execute(input);
+            
+            if (isUseCaseError(output)) {
+                if (output.code === USER_EXCEPTIONS.USER_NOT_FOUND.code) {
+                    return res.status(404).send(output.message);
+                }
+                return res.status(500).send(output.message);
+            }
+
+            return res.send(output.result);
         } catch (error: any) {
             return res.status(500).send(error.message);
         }
