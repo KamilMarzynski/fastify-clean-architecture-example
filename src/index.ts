76c6f72a4c1fd1637e4cc6016c2e185c6972c371
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-05 19:35:16 +0100

    add docker compose

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const server = fastify();
 
 initApp({ server });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ host: "0.0.0.0", port: 8080 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
