afcb3d59c88e5d872dd3ec4ee148b6181fa35e85
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-19 16:56:40 +0100

    Add database configuration to AppConfig

--- src/_lib/core/config.ts
+++ src/_lib/core/config.ts
@@ -1,10 +1,18 @@
 export interface AppConfig {
   port: number
+  db: {
+    url: string
+    dbName: string
+  }
 }
 
 // hardced for now
 export const makeConfig = (): AppConfig => {
   return {
-    port: 8080
+    port: 8080,
+    db: {
+      url: 'mongodb://mongo:27017',
+      dbName: 'project'
+    }
   }
 }
