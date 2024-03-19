946b7bc76f6cf980271ff85afcbdaf20323eb328
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-19 17:20:02 +0100

    Add MongoUserRepository implementation

---
+++ src/app/user/adapters/db/repositories/mongo/user.repository.ts
@@ -0,0 +1,82 @@
+import { ObjectId, type Db, type WithId, type Document } from 'mongodb'
+import { type QueryOptions, type PaginatedResult } from '../../../../../../_lib/core'
+import { User, type UserId } from '../../../../domain'
+import { type UserRepository } from '../../../../ports/repositories/user.repository'
+import { toEntityId } from '../../../../../../_lib/core/entityId'
+
+const dataToEntity = (document: WithId<Document>): User => new User(toEntityId(document._id.toString()), {
+  firstName: document.firstName,
+  lastName: document.lastName,
+  email: document.email
+})
+
+export class MongoUserRepository implements UserRepository {
+  constructor (private readonly db: Db) {}
+
+  async findByEmail (email: string): Promise<User | null> {
+    const document = await this.db.collection('users').findOne({ email })
+
+    if (document === null) {
+      return null
+    }
+
+    return dataToEntity(document)
+  }
+
+  async getNextId (): Promise<UserId> {
+    return toEntityId(new ObjectId().toString())
+  }
+
+  async get (id: UserId): Promise<User | null> {
+    const document = await this.db.collection('users').findOne({ _id: new ObjectId(id.value) })
+
+    if (document === null) {
+      return null
+    }
+
+    return dataToEntity(document)
+  }
+
+  async getAll (query: QueryOptions<User>): Promise<PaginatedResult<User>> {
+    const limit = query.limit ?? 10
+    const page = query.page ?? 1
+    const orderBy = query.orderBy ?? 'id'
+    const order = query.order ?? 'asc'
+
+    const sort = [orderBy, order]
+
+    const data = await this.db.collection('users')
+      .find()
+      .sort(sort)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .toArray()
+
+    const count = await this.db.collection('users').countDocuments()
+
+    return {
+      data: data.map(dataToEntity),
+      pagination: {
+        page,
+        count,
+        maxPage: Math.ceil(count / limit)
+      }
+    }
+  }
+
+  async save (entity: User): Promise<User> {
+    await this.db.collection('users').updateOne({ _id: new ObjectId(entity.id.value) }, {
+      $set: {
+        firstName: entity.props.firstName,
+        lastName: entity.props.lastName,
+        email: entity.props.email
+      }
+    }, { upsert: true })
+
+    return entity
+  }
+
+  async delete (id: UserId): Promise<void> {
+    await this.db.collection('users').deleteOne({ _id: new ObjectId(id.value) })
+  }
+}
