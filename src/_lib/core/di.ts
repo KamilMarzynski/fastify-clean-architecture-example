d08163ed2f90b3c1bba4750499a51f282d833408
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-19 16:57:37 +0100

    Add MongoDB database dependency to dependency container

--- src/_lib/core/di.ts
+++ src/_lib/core/di.ts
@@ -1,17 +1,22 @@
 import { type FastifyInstance } from 'fastify'
 import { makeServer } from '../frameworks/server/server'
 import { type AppConfig } from './config'
+import { type Db } from 'mongodb'
+import { makeDb } from '../frameworks/db/mongo'
 
 export interface AppDependencies {
   server: FastifyInstance
+  db: Db
   config: AppConfig
 }
 
-export const makeDependencyContainer = (deps: { config: AppConfig }): AppDependencies => {
+export const makeDependencyContainer = async (deps: { config: AppConfig }): Promise<AppDependencies> => {
   const server = makeServer()
+  const db = await makeDb(deps)
 
   return {
     server,
+    db,
     config: deps.config
   }
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import { type FastifyInstance } from 'fastify'
 import { UserModule } from './user/user.module'
+import { type Db } from 'mongodb'
+import { type AppConfig } from '../_lib/core/config'
 
 interface AppDependencies {
   server: FastifyInstance
+  db: Db
+  config: AppConfig
 }
 
 export const initApp = (deps: AppDependencies): void => {
