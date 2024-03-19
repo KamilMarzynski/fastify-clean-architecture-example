c5eadef7fb7b5b28416aa61cd61bea0ec279ed3c
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-19 16:57:43 +0100

    Refactor server initialization in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,26 @@ import { initApp } from './app/app'
 import { makeDependencyContainer } from './_lib/core/di'
 import { makeConfig } from './_lib/core/config'
 
-const config = makeConfig()
-const deps = makeDependencyContainer({ config })
+const main = async (): Promise<void> => {
+  const config = makeConfig()
+  const deps = await makeDependencyContainer({ config })
 
-initApp(deps)
+  initApp(deps)
 
-const { server } = deps
+  const { server } = deps
 
-server.listen({ host: '0.0.0.0', port: config.port }, (err, address) => {
-  if (err !== null) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
+  server.listen({ host: '0.0.0.0', port: config.port }, (err, address) => {
+    if (err !== null) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
+  })
+}
+
+main().then(() => {
+  console.log('App started')
+}).catch((err) => {
+  console.error(err)
+  process.exit(1)
 })
