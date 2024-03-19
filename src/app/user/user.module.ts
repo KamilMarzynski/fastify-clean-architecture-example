2e94ebaeefdd5d4d239f7926870d1d34e207a90e
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-19 16:57:22 +0100

    Refactor user module dependencies and adapters

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -9,30 +9,31 @@ import {
   GetUsersUseCaseImpl,
   UpdateUserUseCaseImpl
 } from './use-cases'
+import { type Db } from 'mongodb'
+import { type AppConfig } from '../../_lib/core/config'
 
 interface UserModuleDependencies {
   server: FastifyInstance
+  db: Db
+  config: AppConfig
 }
 
-// TODO: should user module know about fastify?
-// should user module know about the database implementation?
-// maybe I should only inject those to user module and instatiate them higher?
 export class UserModule {
   constructor (private readonly deps: UserModuleDependencies) {}
 
   init (): void {
     const { server } = this.deps
-    // DB adapters
+    // Driven adapters
     const userRepository = new LocalUserRepository()
 
-    // Application use cases
+    // Application
     const createUser = new CreateUserUseCaseImpl(userRepository)
     const findUserById = new FindUserByIdUserUseCaseImpl(userRepository)
     const deleteUser = new DeleteUserUseCaseImpl(userRepository)
     const getUsers = new GetUsersUseCaseImpl(userRepository)
     const updateUser = new UpdateUserUseCaseImpl(userRepository)
 
-    // HTTP adapters
+    // Driving adapters
     const userController = new HttpUserController({
       createUserUseCase: createUser,
       findUserByIdUseCase: findUserById,
@@ -40,11 +41,6 @@ export class UserModule {
       getUsersUseCase: getUsers,
       updateUserUseCase: updateUser
     })
-
-    // TODO: maybe user module shoud export controllers
-    // so that app module can register them
-    // router is tightly bound to the server
-    // but controller is not
     const userRouter = new UserRouter(userController, server)
 
     userRouter.register()
