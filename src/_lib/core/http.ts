a132854b6c29340a5240b873c325e7b2908fd060
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-23 13:47:30 +0100

    Add HttpRoute and HttpController classes

--- src/_lib/core/http.ts
+++ src/_lib/core/http.ts
@@ -11,3 +11,21 @@ export interface Response {
   send: (data: any) => void
   status: (code: number) => Response
 }
+
+export type HttpRoutePath = string
+export type HttpRouteHandler = (req: Request, res: Response) => Promise<void>
+export type HttpRouteMethod = 'get' | 'post' | 'put' | 'delete' | 'patch'
+
+export interface HttpRoute {
+  path: HttpRoutePath
+  method: HttpRouteMethod
+  handler: HttpRouteHandler
+}
+
+export abstract class HttpController {
+  protected routeHandlers: HttpRoute[] = []
+
+  get routes (): HttpRoute[] {
+    return this.routeHandlers
+  }
+}
