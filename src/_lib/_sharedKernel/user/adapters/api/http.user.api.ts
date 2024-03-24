c0e2b96c3ec7ecbcfcec7b150a2245a084bbbf88
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-24 20:35:43 +0100

    Add HTTP user API adapter

---
+++ src/_lib/_sharedKernel/user/adapters/api/http.user.api.ts
@@ -0,0 +1,36 @@
+import http from 'http'
+import { toEntityId } from '../../../../core/entityId'
+import { User, type UserId } from '../../domain'
+import { type UserApi } from '../../ports/api/user.api'
+
+interface UserReadModel {
+  id: string
+  firstName: string
+  lastName: string
+  email: string
+}
+
+export class HttpUserApi implements UserApi {
+  async getUserById (id: UserId): Promise<User> {
+    const data = await new Promise<UserReadModel>((resolve) => {
+      http.get(`http://localhost:3000/users/${id.value}`, (res) => {
+        let data = ''
+
+        res.on('data', (chunk) => {
+          data += chunk
+        })
+
+        res.on('end', () => {
+          resolve(JSON.parse(data) as UserReadModel)
+        })
+      })
+    })
+
+    const userId = toEntityId(data.id)
+    return new User(userId, {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email
+    })
+  }
+}
