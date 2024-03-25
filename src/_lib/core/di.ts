6f4c99dca737c8edf83c790518ecfc880071c465
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-25 19:32:51 +0100

    Refactor dependency container to support different transport types

--- src/_lib/core/di.ts
+++ src/_lib/core/di.ts
@@ -3,23 +3,34 @@ import { makeServer } from '../frameworks/server/server'
 import { type AppConfig } from './config'
 import { type Db } from 'mongodb'
 import { makeDb } from '../frameworks/db/mongo'
-import { type HttpController } from './http'
+import { type Controller } from './controller'
+import { HttpApplicationControllerFactory, type ApplicationControllerFactory } from '../../app/application-controller.factory'
 
 export interface AppDependencies {
   server: FastifyInstance
   db: Db
   config: AppConfig
-  controllers: HttpController[]
+  controllerFactory: ApplicationControllerFactory
+  controllers?: Controller[]
 }
 
 export const makeDependencyContainer = async (deps: { config: AppConfig }): Promise<AppDependencies> => {
   const server = makeServer()
   const db = await makeDb(deps)
 
+  const transportType = deps.config.transportType
+  let applicationControllerFactory: ApplicationControllerFactory
+
+  if (transportType === 'http') {
+    applicationControllerFactory = new HttpApplicationControllerFactory()
+  } else {
+    throw new Error('Unsupported transport type')
+  }
+
   return {
     server,
     db,
     config: deps.config,
-    controllers: []
+    controllerFactory: applicationControllerFactory
   }
 }
