75f9bf7257de4cc8a6793018871a7fad1098d1f0
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-25 19:32:56 +0100

    Refactor server bootstrap to support multiple controller types

--- src/_lib/frameworks/server/server.ts
+++ src/_lib/frameworks/server/server.ts
@@ -1,23 +1,37 @@
 import fastify, { type FastifyInstance } from 'fastify'
 import { type HttpController } from '../../core/http'
 import { type AppConfig } from '../../core/config'
+import { type Controller } from '../../core/controller'
 
 export const makeServer = (): FastifyInstance => {
   return fastify()
 }
 
+const isHttpController = (controller: Controller): controller is HttpController => {
+  return controller.type === 'http'
+}
+
 export const bootstrapServer = (deps: {
   server: FastifyInstance
   config: AppConfig
-  controllers: HttpController[]
+  controllers?: Controller[]
 }): void => {
   const { controllers, server, config } = deps
 
+  if (controllers === undefined) {
+    return
+  }
+
   controllers.forEach(controller => {
-    const routes = controller.routes
-    routes.forEach(route => {
-      server[route.method](route.path, route.handler)
-    })
+    // TODO: only for know, before adding more controller types
+    // move server type to config and then based on config start the right server type
+    // and controllers will then have only this type (eg. http)
+    if (isHttpController(controller)) {
+      const routes = controller.routes
+      routes.forEach(route => {
+        server[route.method](route.path, route.handler)
+      })
+    }
   })
 
   server.listen({ host: '0.0.0.0', port: config.port }, (err, address) => {
