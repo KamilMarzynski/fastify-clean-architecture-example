2860380afb6bcbe01a2dad9aaed0414d618f6486
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-25 19:32:37 +0100

    Add transportType to AppConfig interface

--- src/_lib/core/config.ts
+++ src/_lib/core/config.ts
@@ -1,15 +1,19 @@
+export type TransportType = 'http'
+
 export interface AppConfig {
   port: number
+  transportType: TransportType
   db: {
     url: string
     dbName: string
   }
 }
 
-// hardced for now
+// hardcoded for now
 export const makeConfig = (): AppConfig => {
   return {
     port: 8080,
+    transportType: 'http',
     db: {
       url: 'mongodb://mongo:27017',
       dbName: 'project'
