3f6bf039d3f077f65b6867b1ee76c058a132981f
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-26 06:29:48 +0100

    Add ToDoController and HttpToDoController to ApplicationControllerFactory

--- src/app/application-controller.factory.ts
+++ src/app/application-controller.factory.ts
@@ -1,15 +1,23 @@
 import { HttpUserController } from './user/adapters/controllers/http/user.controller'
 import { type UserControllerFactory } from './user/ports/controllers/user.controller.factory'
 import { type UserControllerDependencies, type UserController } from './user/ports/controllers/user.controller'
+import { type ToDoControllerFactory } from './to-do/ports/controllers/to-do.controller.factory'
+import { type ToDoController, type ToDoControllerDependencies } from './to-do/ports/controllers/to-do.controller'
+import { HttpToDoController } from './to-do/adapters/controllers/http/to-do.controller'
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export abstract class ApplicationControllerFactory implements UserControllerFactory {
+export abstract class ApplicationControllerFactory implements UserControllerFactory, ToDoControllerFactory {
   public abstract createUserController (deps: UserControllerDependencies): UserController
+  public abstract createToDoController (deps: ToDoControllerDependencies): ToDoController
 }
 
 export class HttpApplicationControllerFactory implements ApplicationControllerFactory {
   createUserController (deps: UserControllerDependencies): HttpUserController {
     return new HttpUserController(deps)
+  }
+
+  public createToDoController (deps: ToDoControllerDependencies): HttpToDoController {
+    return new HttpToDoController(deps)
   }
 }
 
