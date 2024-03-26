e04ed2b5f05074b2cec30d143cfdc26c095e4fe9
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-26 07:11:53 +0100

    Refactor application controller factory

--- src/app/application-controller.factory.ts
+++ src/app/application-controller.factory.ts
@@ -5,18 +5,17 @@ import { type ToDoControllerFactory } from './to-do/ports/controllers/to-do.cont
 import { type ToDoController, type ToDoControllerDependencies } from './to-do/ports/controllers/to-do.controller'
 import { HttpToDoController } from './to-do/adapters/controllers/http/to-do.controller'
 
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export abstract class ApplicationControllerFactory implements UserControllerFactory, ToDoControllerFactory {
   public abstract createUserController (deps: UserControllerDependencies): UserController
   public abstract createToDoController (deps: ToDoControllerDependencies): ToDoController
 }
 
 export class HttpApplicationControllerFactory implements ApplicationControllerFactory {
-  createUserController (deps: UserControllerDependencies): HttpUserController {
+  public createUserController (deps: UserControllerDependencies): UserController {
     return new HttpUserController(deps)
   }
 
-  public createToDoController (deps: ToDoControllerDependencies): HttpToDoController {
+  public createToDoController (deps: ToDoControllerDependencies): ToDoController {
     return new HttpToDoController(deps)
   }
 }
