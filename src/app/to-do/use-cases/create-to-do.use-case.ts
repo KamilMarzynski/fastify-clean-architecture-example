fbc57aaa9eeaa2193b3c7013bb837f194638a185
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-26 06:29:14 +0100

    Add user validation in CreateToDoUseCase

--- src/app/to-do/use-cases/create-to-do.use-case.ts
+++ src/app/to-do/use-cases/create-to-do.use-case.ts
@@ -6,14 +6,21 @@ import {
 } from '../ports/use-cases'
 import { type TodoRepository } from '../ports/repositories/to-do.repository'
 import { ToDo } from '../domain'
+import { type UserApi } from '../../../_lib/_sharedKernel/user/ports'
+import { TO_DO_EXEPCTIONS } from './exceptions'
 
 export class CreateUserUseCaseImpl implements CreateToDoUseCase {
-  constructor (private readonly todoRepository: TodoRepository) {}
+  constructor (private readonly todoRepository: TodoRepository, private readonly userApi: UserApi) {}
 
   async execute (input: CreateToDoInput): Promise<CreateToDoOutput> {
     const { data } = input
 
     const ownerId = toEntityId(data.ownerId)
+    const owner = await this.userApi.getUserById(ownerId)
+    if (owner === null) {
+      return TO_DO_EXEPCTIONS.USER_NOT_FOUND
+    }
+
     const id = await this.todoRepository.getNextId()
     const toDo = ToDo.create(id, {
       title: data.title,
