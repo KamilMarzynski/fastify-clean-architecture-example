5d142aa86e4d8293d234d40c321c7b02cdfbb4ac
KamilMarzynski <kamil.marzynski@gmail.com>
2024-03-28 07:22:36 +0100

    Add ToDoModule to initApp function

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { UserModule } from './user/user.module'
 import { type AppDependencies } from '../_lib/core/di'
+import { ToDoModule } from './to-do/to-do.module'
 
 export const initApp = (deps: AppDependencies): void => {
   const userModule = new UserModule(deps)
+  const toDoModule = new ToDoModule(deps)
   const userModuleProviders = userModule.init()
+  const toDoModuleProviders = toDoModule.init()
 
-  deps.controllers = [...userModuleProviders.controllers]
+  deps.controllers = [...userModuleProviders.controllers, ...toDoModuleProviders.controllers]
 }
