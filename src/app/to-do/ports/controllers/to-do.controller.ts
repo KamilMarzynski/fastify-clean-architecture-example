118a82c2c7f5e86916febe350b97f838919c3573
KamilMarzynski <kamil.marzynski@gmail.com>
2024-04-03 20:01:32 +0200

    Add completeToDo and findToDoById methods to ToDoController

--- src/app/to-do/ports/controllers/to-do.controller.ts
+++ src/app/to-do/ports/controllers/to-do.controller.ts
@@ -1,10 +1,14 @@
 import { type Controller } from '../../../../_lib/core'
-import { type CreateToDoUseCase } from '../use-cases'
+import { type CompleteToDoUseCase, type CreateToDoUseCase, type FindToDoByIdUseCase } from '../use-cases'
 
 export interface ToDoControllerDependencies {
   createToDoUseCase: CreateToDoUseCase
+  completeToDoUseCase: CompleteToDoUseCase
+  findToDoByIdUseCase: FindToDoByIdUseCase
 }
 
 export interface ToDoController extends Controller {
   createToDo: (...args: any[]) => Promise<any>
+  completeToDo: (...args: any[]) => Promise<any>
+  findToDoById: (...args: any[]) => Promise<any>
 }
