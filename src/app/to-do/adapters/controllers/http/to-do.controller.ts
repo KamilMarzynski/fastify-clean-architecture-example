e72201966630daaf84f72104d0401b8991d23c97
KamilMarzynski <kamil.marzynski@gmail.com>
2024-04-03 20:02:05 +0200

    Add completeToDo and findToDoById methods to HttpToDoController

--- src/app/to-do/adapters/controllers/http/to-do.controller.ts
+++ src/app/to-do/adapters/controllers/http/to-do.controller.ts
@@ -2,15 +2,19 @@ import { HttpController, type Request, type Response } from '../../../../../_lib
 import { isUseCaseError } from '../../../../../_lib/core/use-case'
 import { type ToDoController, type ToDoControllerDependencies } from '../../../ports/controllers/to-do.controller'
 
-import { type CreateToDoUseCase } from '../../../ports/use-cases'
+import { type CompleteToDoUseCase, type FindToDoByIdUseCase, type CreateToDoUseCase } from '../../../ports/use-cases'
 import { TO_DO_EXCEPTIONS } from '../../../use-cases/exceptions'
 
 export class HttpToDoController extends HttpController implements ToDoController {
   protected readonly createToDoUseCase: CreateToDoUseCase
+  protected readonly completeToDoUseCase: CompleteToDoUseCase
+  protected readonly findToDoByIdUseCase: FindToDoByIdUseCase
 
   constructor (deps: ToDoControllerDependencies) {
     super()
     this.createToDoUseCase = deps.createToDoUseCase
+    this.findToDoByIdUseCase = deps.findToDoByIdUseCase
+    this.completeToDoUseCase = deps.completeToDoUseCase
 
     // TODO: move to decorators for each method
     this.handlers = [
@@ -18,8 +22,68 @@ export class HttpToDoController extends HttpController implements ToDoController
         method: 'post',
         path: '/to-do',
         handler: this.createToDo
+      },
+      {
+        method: 'put',
+        path: '/to-do/:id/complete',
+        handler: this.createToDo
+      },
+      {
+        method: 'get',
+        path: '/to-do/:id',
+        handler: this.createToDo
       }
     ]
+  }
+
+  public readonly completeToDo = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const input = {
+        data: req.body
+      }
+      const output = await this.completeToDoUseCase.execute(input)
+
+      if (isUseCaseError(output)) {
+        if (output.code === TO_DO_EXCEPTIONS.TODO_NOT_FOUND.code) {
+          // TODO: better error return
+          res.status(404).send(output.message)
+          return
+        }
+        res.status(500).send(output.message)
+        return
+      }
+
+      res.send({
+        toDoId: output.result.id
+      })
+    } catch (error: any) {
+      res.status(500).send(error.message)
+    }
+  }
+
+  public readonly findToDoById = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const input = {
+        data: req.body
+      }
+      const output = await this.findToDoByIdUseCase.execute(input)
+
+      if (isUseCaseError(output)) {
+        if (output.code === TO_DO_EXCEPTIONS.TODO_NOT_FOUND.code || output.code === TO_DO_EXCEPTIONS.USER_NOT_FOUND.code) {
+          // TODO: better error return
+          res.status(404).send(output.message)
+          return
+        }
+        res.status(500).send(output.message)
+        return
+      }
+
+      res.send({
+        toDoId: output.result.id
+      })
+    } catch (error: any) {
+      res.status(500).send(error.message)
+    }
   }
 
   public readonly createToDo = async (req: Request, res: Response): Promise<void> => {
