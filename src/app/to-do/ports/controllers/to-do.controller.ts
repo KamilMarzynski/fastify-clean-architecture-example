f6f2dda956b0ee41a90f699965da093101385105
KamilMarzynski <kamil.marzynski@gmail.com>
2024-04-04 18:59:23 +0200

    Add deleteToDo method to ToDoController

--- src/app/to-do/ports/controllers/to-do.controller.ts
+++ src/app/to-do/ports/controllers/to-do.controller.ts
@@ -1,14 +1,16 @@
 import { type Controller } from '../../../../_lib/core'
-import { type CompleteToDoUseCase, type CreateToDoUseCase, type FindToDoByIdUseCase } from '../use-cases'
+import { type DeleteToDoUseCase, type CompleteToDoUseCase, type CreateToDoUseCase, type FindToDoByIdUseCase } from '../use-cases'
 
 export interface ToDoControllerDependencies {
   createToDoUseCase: CreateToDoUseCase
   completeToDoUseCase: CompleteToDoUseCase
   findToDoByIdUseCase: FindToDoByIdUseCase
+  deleteToDoUseCase: DeleteToDoUseCase
 }
 
 export interface ToDoController extends Controller {
   createToDo: (...args: any[]) => Promise<any>
   completeToDo: (...args: any[]) => Promise<any>
   findToDoById: (...args: any[]) => Promise<any>
+  deleteToDo: (...args: any[]) => Promise<any>
 }
